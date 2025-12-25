e2980b08d4d765ed18f64c53f59044c33a1939a5
Pankhudi Srivastava <pankhudi.sriv@gmail.com>
2025-12-25 15:14:51 +0530

    Initial Razorpay payment gateway integration

---
+++ src/main/java/com/spring/implementation/RazorpayPaymentGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.spring.implementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazorpayPaymentGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RazorpayPaymentGatewayApplication.class, args);
+    }
+}
